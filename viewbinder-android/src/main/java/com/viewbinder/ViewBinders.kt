b38a377767ea549e4f3273e7af063b63ec5a96eb
Dmitry Borodin <inbox@DmitryBorodin.com>
2017-11-28 12:15:06 +0100

    cleaning up

--- viewbinder-android/src/main/java/com/viewbinder/ViewBinders.kt
+++ viewbinder-android/src/main/java/com/viewbinder/ViewBinders.kt
@@ -10,16 +10,17 @@ import android.app.Fragment
 import android.support.annotation.IdRes
 import android.view.View
 
-fun<T : View> Activity.bindView(@IdRes id: Int): Lazy<T> = lazy(LazyThreadSafetyMode.NONE) { findViewById<T>(id) }
+fun <T : View> Activity.bindView(@IdRes id: Int): Lazy<T> = lazy(LazyThreadSafetyMode.NONE) { findViewById<T>(id) }
 
-fun<T : View> Fragment.abstractBind(@IdRes id: Int, lazyManager: ResettableLazyManager)
+fun <T : View> Fragment.abstractBind(@IdRes id: Int, lazyManager: ResettableLazyManager)
         : ResettableLazy<T> = ResettableLazy(lazyManager) { view.findViewById<T>(id) }
-fun <T : View> android.support.v4.app.Fragment.bindColor(id: Int)
-        : Lazy<T> = lazy(LazyThreadSafetyMode.NONE) { context.getColor(id)) }
 
-fun <T : View> View.bindView(id: Int) : Lazy<T> = lazy(LazyThreadSafetyMode.NONE) { rootView.findViewById<T>(id) }
+fun Fragment.bindColor(id: Int)
+        : Lazy<Int> = lazy(LazyThreadSafetyMode.NONE) { context.getColor(id) }
 
-fun <T : View> Dialog.bindView(id: Int) : Lazy<T> = lazy(LazyThreadSafetyMode.NONE) { findViewById<T>(id) }
+fun <T : View> View.bindView(id: Int): Lazy<T> = lazy(LazyThreadSafetyMode.NONE) { rootView.findViewById<T>(id) }
+
+fun <T : View> Dialog.bindView(id: Int): Lazy<T> = lazy(LazyThreadSafetyMode.NONE) { findViewById<T>(id) }
 
 //private fun viewNotFound(id:Int, desc: KProperty<*>): Nothing =
 //        throw IllegalStateException("View ID $id for '${desc.name}' not found.")
